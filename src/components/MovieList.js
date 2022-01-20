a21b3db719ccf81f25029947b740441d285a3731
Douglas Wilson <dougwilson0000@gmail.com>
2022-01-20 03:43:48 -0500

    blazing through the coursework for module 2. Just coded mapStateToProps for three components

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
 
 const MovieList = (props)=> {
-    const movies = [];
+   
+    const movies = props.movies;
 
+
+    
+   
     return (
         <div className="col">
             <table className="table table-striped table-hover">
@@ -31,4 +35,11 @@ const MovieList = (props)=> {
     );
 }
 
-export default MovieList;
+const mapStateToProps = (state) => {
+    
+    return {
+        movies: state.movies
+    }
+
+};
+export default connect(mapStateToProps)(MovieList);
